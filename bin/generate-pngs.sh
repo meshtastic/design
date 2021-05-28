529b86592e4ad03bb7abca698552bbe3503e03ef
Kevin Hester <kevinh@geeksville.com>
2021-05-28 09:54:51 +0800

    add amazon store icon

--- bin/generate-pngs.sh
+++ bin/generate-pngs.sh
@@ -8,3 +8,4 @@ echo "Regenerating PNGs from SVGs"
 inkscape --batch-process -o $DESTDIR/Mesh_Logo_Black_Small.png -w 50 -h 28 $SRCDIR/Mesh_Logo_Black_Small.svg
 
 inkscape --batch-process -o $DESTDIR/play_store_icon.png -w 512 -h 512 logo/inkscape/play_store_icon.svg
+inkscape --batch-process -o $DESTDIR/play_store_icon_114px.png -w 114 -h 114 logo/inkscape/play_store_icon.svg
